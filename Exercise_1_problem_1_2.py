d0c0bde0bddbae7e3fe2309f27657ef702c5fe2a
ガイア <>
2021-06-04 12:55:08 +0000

    I add codes about create_poly_geom

--- Exercise_1_problem_1_2.py
+++ Exercise_1_problem_1_2.py
@@ -37,6 +37,7 @@ def create_line_geom(points):
 
 # YOUR CODE HERE 3 to define two points and store the result in line1
 demopoint=[(45.2,22.34),(100.22,-3.2)]
+print("demopointは",type(demopoint))
 line1 =LineString(demopoint)
 
 # CODE FOR TESTING YOUR SOLUTION
@@ -69,16 +70,18 @@ except Exception as e:
 
 # YOUR CODE HERE 4 to define create_poly_geom()
 def create_poly_geom(coords):
-  assert type(coords)=="list","Input sholudbe a list!"
+  assert type(coords) is list,"Input sholud be a list!"
+  
   assert len(coords)>=3,"Polygon objectrequires at least three Points!"
   for i in coords:
-    assert type(i)=="tuple","All list values should be coordinate tuples!"
+    assert type(i) is tuple,"All list values should be coordinate tuples!"
   return Polygon(coords)
 
 # Demonstrate the usage of the function. For example, create a Polygon with three points: `(45.2, 22.34)`, `(100.22, -3.20)` & `(70.0, 10.20)`.
 
 # YOUR CODE HERE 5 to define poly1 with three points
-democoords=[(45.2,22.34),(100.22,22.34),(70.0,10.20)]
+democoords=[(45.2,22.34),(100.22,-3.20),(70.0,10.20)]
+
 poly1 = create_poly_geom(democoords)
 
 # CODE FOR TESTING YOUR SOLUTION
