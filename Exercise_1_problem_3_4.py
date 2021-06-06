442082916c5e7c014185d5bd1e6e3425530c82d7
ガイア <>
2021-06-06 14:49:12 +0000

    I cerated shapely points for each origin and destination and added origin points

--- Exercise_1_problem_3_4.py
+++ Exercise_1_problem_3_4.py
@@ -70,7 +70,11 @@ print('dest_points length:', len(dest_points))
 
 # YOUR CODE HERE 4 to append points in orig_points and dest_points
 from shapely.geometry import Point
-
+for i in range(len(data)):
+  orig_point=Point(data['from_x'][i],data[['from_y'][i]])
+  dest_point=Point(data['to_x'][i],data['to_y'][i])
+  orig_points.append(orig_point)
+  dest_points.append(dest_point)
 # CODE FOR TESTING YOUR SOLUTION
 
 # This test print should print out the first origin and destination coordinates in the two lists:
